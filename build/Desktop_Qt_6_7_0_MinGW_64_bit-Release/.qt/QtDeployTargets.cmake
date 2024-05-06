98db30e23a503534156e4552a05e48e9c7383e5f
Rafael dos Santos <rafaels1997@LIVE.COM>
2024-05-06 19:29:13 -0300

    envio do exe

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Compilador_aula_FILE C:/Users/Rafael/Documents/Compilador_aula/build/Desktop_Qt_6_7_0_MinGW_64_bit-Release/Compilador_aula.exe)
-set(__QT_DEPLOY_TARGET_Compilador_aula_RUNTIME_DLLS E:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
